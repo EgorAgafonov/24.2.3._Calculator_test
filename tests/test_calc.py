a4fc83855d9effa260b4fe66a8c2d68c9402fd33
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2023-12-14 17:00:40 +0300

    Files updated

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -6,6 +6,8 @@ calculator = Calculator()
 
 
 class TestCalculator:
+    """Комплект тестов для модульного(unit) тестирования класса Calculator."""
+
     @time_of_test
     def test_multiply_success(self):
         """Выполняем позитивный тест для проверки корректной работы метода multiply, для чего передаем
@@ -15,6 +17,7 @@ class TestCalculator:
         result = calculator.multiply(2, 2)
         assert result == 4
 
+    @time_of_test
     def test_division_success(self):
         """Выполняем позитивный тест для проверки корректной работы метода division, для чего передаем
         необходимые числовые аргументы для деления. Полученный результат частного сравниваем с ожидаемым
@@ -23,6 +26,7 @@ class TestCalculator:
         result = calculator.division(8, 8)
         assert result == 1
 
+    @time_of_test
     def test_subtraction_success(self):
         """Выполняем позитивный тест для проверки корректной работы метода subtraction, для чего передаем
         необходимые числовые аргументы для вычитания. Полученный результат разности чисел сравниваем
@@ -31,19 +35,25 @@ class TestCalculator:
         result = calculator.subtraction(5, 5)
         assert result == 0
 
+    @time_of_test
     def test_adding_success(self):
         """Выполняем позитивный тест для проверки корректной работы метода adding, для чего передаем
         необходимые числовые аргументы для сложения. Полученный результат суммы чисел сравниваем
         с ожидаемым посредством инструкции assert."""
+
         result = calculator.adding(10, 10)
         assert result == 20
 
-    def test_adding_unsuccess(self):
+    def test_adding_unsuccessful(self):
         """Выполняем негативный тест для проверки корректной работы метода adding, для чего передаем необходимые
         числовые аргументы для сложения, а также для сравнения намеренно указываем некорректный ожидаемый результат."""
-        assert calculator.adding(1, 1) == 3
 
+        assert calculator.adding(1, 1) == 3, (f"\nНегативный тест успешно пройден, ожидаемый результат != "
+                                              f"фактическому.")
+
+    @time_of_test
     def test_zero_division(self):
         """Добавим позитивный тест проверки срабатывания исключения ZeroDivisionError при делении на 0."""
+
         with pytest.raises(ZeroDivisionError):
-            calculator.division(1, 0)
+            result = calculator.division(1, 0)
